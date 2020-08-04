cf19fde5d9e6e7a27ef86c49ad8cfeb600f234cd
David Ionita <david@ionita.dev>
2020-08-03 22:13:42 -0400

    getRiderOffers

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mango_driver/models/rider_offer.dart';
 
 class FirestoreService {
   Firestore _db = Firestore.instance;
+  Geoflutterfire geo = Geoflutterfire();
 
   Future<void> addRiderOffer(
       RiderOffer initialOffer, BuildContext context) async {
@@ -23,5 +26,11 @@ class FirestoreService {
 //    print("print response:");
 //    print(resp);
 //    print(resp.runtimeType);
+  }
+
+  Stream<List<RiderOffer>> getRiderOffers(LatLng driverLocation) {
+    var queryRef = _db.collection('riderOffers').where('accepted', isEqualTo: false);
+    GeoFirePoint center = geo.point(latitude: driverLocation.latitude, longitude: driverLocation.longitude);
+    return geo.collection(collectionRef: queryRef).within(center: center, radius: 30, field: 'position');
   }
 }
