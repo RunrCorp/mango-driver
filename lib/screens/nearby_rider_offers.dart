980ed6685459d2a6ff1cd1b3234aa2c8bb130bef
David Ionita <david@ionita.dev>
2020-08-06 21:44:39 -0400

    typo fix

--- lib/screens/nearby_rider_offers.dart
+++ lib/screens/nearby_rider_offers.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_maps_flutter/google_maps_fluttoffer.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mango_driver/services/firestore_service.dart';
 import 'package:provider/provider.dart';
 
